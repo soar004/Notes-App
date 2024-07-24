a976e9cdaeacc8f661528f665214d3f6001a1cc8
Solveig Årvik <soar004@student.kristiania.no>
2024-07-25 00:28:39 +0200

    added absolute positioning to every card

--- notes-app/src/components/NoteCard.jsx
+++ notes-app/src/components/NoteCard.jsx
@@ -6,7 +6,14 @@ export const NoteCard = ({ note }) => {
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
   return (
-    <div className="card" style={{ backgroundColor: colors.colorBody }}>
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
