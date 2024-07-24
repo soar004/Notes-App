1e176871e6f312cc512e70438e77c069172ae66d
Solveig Årvik <soar004@student.kristiania.no>
2024-07-25 00:23:50 +0200

    added textbox features and trash icon

--- notes-app/src/components/NoteCard.jsx
+++ notes-app/src/components/NoteCard.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-const NoteCard = ({ note }) => {
+export const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
   return (
     <div className="card" style={{ backgroundColor: colors.colorBody }}>
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      ></div>
+
+      <div className="card-body">
+        <textarea
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+        ></textarea>
+      </div>
+
       {body}
     </div>
   );
