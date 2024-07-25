ac95f85a5b64455487cc5a645971c11da5158787
Solveig Årvik <soar004@student.kristiania.no>
2024-07-25 17:21:31 +0200

    added functionality for deleting notes in database

--- notes-app/src/components/DeleteButton.jsx
+++ notes-app/src/components/DeleteButton.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Trash from "../icons/Trash";
+import { db } from "../appwrite/databases";
 
 const DeleteButton = ({ noteId, setNotes }) => {
-  const handleDelete = async (e) => {
+  const handleDelete = async () => {
+    db.notes.delete(noteId);
     setNotes((prevState) => prevState.filter((note) => note.$id !== noteId));
   };
   return (
