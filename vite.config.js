80252204372892a53ba3ba0e7b5ac4b7400f1e1a
Solveig Årvik <soar004@student.kristiania.no>
2024-07-26 17:14:06 +0200

    trying to deploy differently

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/soar004/Notes-App/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
